02921c4494ce10fca8116b8d42032f317f3ec489
Victor Samson <victorsamson1168@gmail.com>
2020-07-26 19:06:22 +0530

    not working

--- sc.js
+++ sc.js
@@ -1,10 +1,50 @@
 let redcir=document.getElementById("redp");
 let greencir=document.getElementById("grep");
 let yellowcir=document.getElementById("yelp");
+let car=document.getElementById("car");
+// let carplacetop=car.offsetTop;
+// let carplaceleft=car.offsetLeft;
+// let carplace=car.offsetHeight;
+// let carplacetop=car.offsetWidth;
 const ti=500;
+let rand=Math.floor((Math.random()*3)+1);
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    if (e.keyCode == '38') {
+        console.log("up arrow"+"   "+rand);
+        car.style.top=(parseInt(car.style.top)-5)+'px';
+        console.log(car.style.top+"  ");
+
+        
+
+    }
+    else if (e.keyCode == '40') {
+        console.log("down arrow"+"   "+rand);
+        car.style.top=(parseInt(car.style.top)+5)+'px';
+        console.log(car.style.top+"  ");
+
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+        console.log("left arrow"+"   "+rand);
+        car.style.left=(parseInt(car.style.left)-5)+'px';
+
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+        console.log("right arrow"+"   "+rand);
+        car.style.left-=(parseInt(car.style.left)+5)+'px';
+
+       // right arrow
+    }
+
+}
+
 
  function changcolor(){
-    let rand=Math.floor((Math.random()*3)+1);
+    rand=Math.floor((Math.random()*3)+1);
     if(rand===1)
     {
         
